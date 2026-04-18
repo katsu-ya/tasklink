990407cadf7ba631d2d05581d3aae4107da5349b
katsu-ya <shinjokatsuya@icloud.com>
2026-04-19 05:24:16 +0900

    feat: UX改善（フラッシュ自動消去など）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+console.log("JS読み込まれた");
+
+document.addEventListener("turbo:load", () => {
+  console.log("turbo:load発火");
+
+  const flashes = document.querySelectorAll('.flash');
+
+  if (flashes.length === 0) return;
+
+  setTimeout(() => {
+    flashes.forEach(el => {
+      el.style.transition = "opacity 0.5s";
+      el.style.opacity = "0";
+
+      setTimeout(() => {
+        el.remove();
+      }, 500);
+    });
+  }, 3000);
+});
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
