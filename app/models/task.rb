e823e5db9c261f75c72ba929b7dcac9b9abdba1b
katsu-ya <shinjokatsuya@icloud.com>
2026-05-21 06:04:44 +0900

    Add task model test

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :team, optional: true
 
+  validates :title, presence: true
+
   enum :status, { todo: 0, doing: 1, done: 2 }
 end
+
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "task is valid with title" do
+    task = Task.new(
+      title: "Study Rails",
+      user: users(:one)
+    )
+
+    assert task.valid?
+
+  end
+
+  test "task is invalid without title" do
+    task = Task.new(
+      title: nil,
+      user: users(:one)
+    )
+
+    assert_not task.valid?
+
+  end
 end
+
+
