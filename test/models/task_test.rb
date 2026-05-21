926ebd8e975a71f94fe34888c4e22d9a4c4b484a
katsu-ya <shinjokatsuya@icloud.com>
2026-05-22 04:45:48 +0900

    Add task model tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -17,5 +17,54 @@ class TaskTest < ActiveSupport::TestCase
     )
 
     assert_not task.valid?
+   end
+
+  test "task is invalid without user" do
+    task = Task.new(
+      title: "Study Rails",
+      user: nil
+    )
+
+    assert_not task.valid?
+  end
+
+  test "task status can be todo" do
+    task = Task.new(
+      title: "Study Rails",
+      user: users(:one),
+      status: :todo
+    )
+
+    assert task.valid?
+  end
+
+  test "task status can be doing" do
+    task = Task.new(
+      title: "Study Rails",
+      user: users(:one),
+      status: :doing
+    )
+
+    assert task.valid?
+  end
+
+  test "task status can be done" do
+    task = Task.new(
+      title: "Study Rails",
+      user: users(:one),
+      status: :done
+    )
+
+    assert task.valid?
+  end
+
+  test "task is valid without team" do
+    task = Task.new(
+      title: "Study Rails",
+      user: users(:one),
+      team: nil
+    )
+
+    assert task.valid?
   end
 end
