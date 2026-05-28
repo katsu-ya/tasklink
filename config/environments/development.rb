eb5f347b9d57356a85b4d504ef85a80edbc83435
katsu-ya <shinjokatsuya@icloud.com>
2026-05-29 05:17:40 +0900

    docs: add Docker setup section to README

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
+  config.exceptions_app = self.routes
 
   # Enable server timing.
   config.server_timing = true
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
