4cf8ba6c10306bf991fdaef8eba17239b2c7c7ae
katsu-ya <shinjokatsuya@icloud.com>
2026-06-12 06:25:29 +0900

    スクショ更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   belongs_to :team, optional: true
 
   has_many :tasks, dependent: :destroy
+
+  after_create :create_default_team
+
+  private
+
+  def create_default_team
+    team = Team.create!(
+      name: "#{email.split('@').first}のチーム"
+    )
+
+    update_column(:team_id, team.id)
+  end
 end
